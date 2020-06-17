939a1daf243c46d42e3c758ce78cc4dec5f6b12f
Johan Cañas <jxexcxo@gmail.com>
2020-06-16 21:09:54 -0500

    se continua back

--- playzoneb/home/models.py
+++ playzoneb/home/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+categoria = (
+    ('Juegos de accion', 'Juegos de accion'),
+    ('Juegos de simulacion', 'Juegos de simulacion'),
+    ('Juegos de deportes', 'Juegos de deportes'),
+    ('Juegos de aventura', 'Juegos de aventura'),
+    ('Juegos de plataformas', 'Juegos de plataformas'),
+    ('Juegos de puzzle', 'Juegos de puzzle'),
+)
+class Juego(models.Model):
+    nombre = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='home/', null=True)
+    descripcion = models.TextField()
+    precio = models.PositiveIntegerField()
+    categoria = models.CharField(choices=categoria, max_length=80, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Juego'
+        verbose_name_plural = 'Juegos'
--- playzoneb/home/views.py
+++ playzoneb/home/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from .models import Juego
 # Create your views here.
 def home(request):
+    lista_juegos = Juego.objects.all()
     return render(request, 'home.html', {
+    'lista_juegos': lista_juegos,
+    })
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('/')
+
+    return render(request, 'login.html', {
         
     })
